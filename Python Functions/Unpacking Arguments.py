8e46ee7bd9369f363d612443fc855b0758516978
Brandon Estevez <bestevez32@gmail.com>
2016-01-27 13:01:52 -0800

    Delete Unpacking Arguments.py

--- Python Functions/Unpacking Arguments.py
+++
@@ -1,8 +0,0 @@
-def currency_value_comparison(value, value_increase, value_depreciation):
-    answer = value + (value_increase * 3.25) - value_depreciation
-    print(answer)
-
-usd_to_yen = [123.36, 0.008071, 122.36]
-
-currency_value_comparison(*usd_to_yen)
-
