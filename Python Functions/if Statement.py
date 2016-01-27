38299ae004a572efa754ec45d141163407b14fce
Brandon Estevez <bestevez32@gmail.com>
2016-01-27 13:02:02 -0800

    Delete if Statement.py

--- Python Functions/if Statement.py
+++
@@ -1,8 +0,0 @@
-age = 45
-
-if age < 20:
-    print("Don't waste your time!")
-elif age > 20:
-    print("One more year before you can buy beers!")
-else:
-    print("We should go to the pub and play darts!")
